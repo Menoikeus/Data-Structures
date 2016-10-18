1e403bcaaf60efc90bbc02634dc53f48b02f19e8
Menoikeus <commander1347@hotmail.com>
2016-10-18 12:19:42 -0400

    Worked on AminoDN
    Almost done. Haven't outputed to file yet

--- AminoDN/src/AminoDN.java
+++ AminoDN/src/AminoDN.java
@@ -9,37 +9,93 @@ public class AminoDN {
 		FileInput inFile = new FileInput("H:\\DNA\\test.txt");
 		FileOutput outFile = new FileOutput("H:\\DNA\\AfterDN.txt");
 		
-		ArrayList<String> translations = new ArrayList<String>();
+		ArrayList<String> translations = new ArrayList<String>(); //array list of the translations in the file
 		
-		int count = 0;
+		//line number of where start and stop appear in the file
+		int indexOfStarts = 0;
+		int indexOfEnds = 0;
+		
 		while(translationFile.hasMoreLines())
 		{
-			String s = translationFile.readLine();
-			String formattedS = s.replace("\t", "*");
-			
-			translations.add(formattedS);
-				
-			count++;
+			String formattedS = translationFile.readLine().replace("\t", "*");
+			translations.add(formattedS); //add formatted strings to translations
 		}
 		
+		//find where the start and stop translation lines are
 		for(int i = 0; i < translations.size(); i++)
 		{
-			System.out.println(translations.get(i));
+			String[] vals = extractCodons(translations.get(i)); 
+			if(vals[0].equals("START"))
+				indexOfStarts = i;
+			else if(vals[0].equals("STOP"))
+				indexOfEnds = i;
 		}
 		
-		/*
+		ArrayList<String> translatedLines = new ArrayList<String>(); //result from translations
 		while(inFile.hasMoreLines())
 		{
-			String s = inFile.readLine();
+			String s = inFile.readLine(); //read in line
 			
-			int start = s.indexOf(translations.get(translations.size()-2).get(0));
-			int codonCount = 0;
-			String aminoSequence;
-			boolean isReading = true;
-			while(isReading)
+			boolean stillInLine = true; //since there can be multiple chains in one line, stay in the line until
+										//we've reached the end
+			int currentLineStart = 0;   //once a chain is read, we're going to cut off that part and read what's after
+			String aminoAcids = "";		//this is what's getting added to the translationedlines arraylist
+			while(stillInLine)
 			{
-				String codon = s.substring(codonCount * 3 + start, codonCount * 3 + start + 3);
+				s = s.substring(currentLineStart);	//cut the line from where we left off (0 if we're just starting)
+				int lineStart = -1;		//this'll be the index of where to start reading (when we see the first START)
+				for(String codon : extractCodons(translations.get(indexOfStarts)))
+				{
+					if(s.indexOf(codon) != -1 && lineStart == -1)
+						lineStart = s.indexOf(codon);
+				}
+				
+				if(lineStart != -1)	//if we actual start
+				{
+					boolean stillReading = true;	//keep reading codons until we get to end of line or a STOP codon
+					int codonCount = 1;				//count of how many codons have been read
+					String acidAddition = "";		//the amino chain we're going to be adding to the translated line
+					while(stillReading)
+					{
+						String codon = s.substring(lineStart + 3 * codonCount, lineStart + 3 * codonCount + 3); //get the next codon in the chain
+						
+						String val = "";	//value will store the translated values
+						for(int z = translations.size()-1; z >= 0; z--)		//traverse from the back, so that we can see if it stops first
+							if(translations.get(z).indexOf(codon) != -1 && val == "" && z != indexOfStarts)		//if the codon exists in one of the 
+								val = extractCodons(translations.get(z))[0];									//translation lines, set 'val' to the value before the *. Also make sure not to read anything as a 'START'
+						if(val.equals("STOP"))
+							stillReading = false; //if it's a stop, get out of reading
+						else
+							acidAddition += val;  //else, add the codon to the current chain
+
+						codonCount++;			//increment the number of codons we've counted, so we can count the next one
+						if(!(lineStart + 3 * codonCount + 3 <= s.length()))
+							stillReading = false; //if we're out of bounds, stop
+					}
+					if(acidAddition.equals(""))		//if the acid addition was empty, we've got an START-STOP or START-END
+						aminoAcids += "NULL";
+					else							//else add the current chain to the current line
+						aminoAcids += acidAddition;
+					aminoAcids += " ";				//space after
+					
+					currentLineStart = lineStart + 3 * codonCount;	//we're going to cut out the chain we just read and everything before it
+																	//this is the index of the next codon
+				}
+				else
+					stillInLine = false;	//if we didn't find any START, end the line read
+				if(currentLineStart + 2 > s.length())
+					stillInLine = false;
 			}
-		}*/
+			translatedLines.add(aminoAcids);
+		}
+
+		for(int j = 0; j < translatedLines.size(); j++)
+			System.out.println(j + ": " + translatedLines.get(j));
+	}
+	
+	public static String[] extractCodons(String translations)
+	{
+		return translations.split("[*,]"); //splits the lines and finds the values
 	}
 }
+
