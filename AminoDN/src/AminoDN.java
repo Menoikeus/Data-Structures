5dfa2cd653fc1bf15ce75c43cdf0a3f7cdef34a6
Menoikeus <commander1347@hotmail.com>
2016-10-24 14:27:09 -0400

    Fixed comment overflow

--- AminoDN/src/AminoDN.java
+++ AminoDN/src/AminoDN.java
@@ -32,19 +32,26 @@ public class AminoDN {
 				indexOfStarts = i;
 		}
 		
-		ArrayList<String> translatedLines = new ArrayList<String>(); //result from translations
+		//result from translations
+		ArrayList<String> translatedLines = new ArrayList<String>(); 
 		while(inFile.hasMoreLines())
 		{
 			String s = inFile.readLine(); //read in line
 			
-			boolean stillInLine = true; //since there can be multiple chains in one line, stay in the line until
+			boolean stillInLine = true; //since there can be multiple chains
+										//in one line, stay in the line until
 										//we've reached the end
-			int currentLineStart = 0;   //once a chain is read, we're going to cut off that part and read what's after
-			String aminoAcids = "";		//this is what's getting added to the translationedlines arraylist
+			int currentLineStart = 0;   //once a chain is read, we're going to 
+										//cut off that part and read what's after
+			String aminoAcids = "";		//this is what's getting added to 
+										//the translationedlines arraylist
 			while(stillInLine)
 			{
-				s = s.substring(currentLineStart);	//cut the line from where we left off (0 if we're just starting)
-				int lineStart = -1;		//this'll be the index of where to start reading (when we see the first START)
+				s = s.substring(currentLineStart);	//cut the line from where 
+													//we left off 
+													//(0 if we're just starting)
+				int lineStart = -1;		//this'll be the index of where to start
+										//reading (when we see the first START)
 				for(String codon : extractCodons(translations.get(indexOfStarts)))
 				{
 					if(s.indexOf(codon) != -1 && lineStart == -1)
@@ -53,47 +60,62 @@ public class AminoDN {
 				
 				if(lineStart != -1)	//if we actual start
 				{
-					boolean stillReading = true;	//keep reading codons until we get to end of line or a STOP codon
-					int codonCount = 1;				//count of how many codons have been read
-					String acidAddition = "";		//the amino chain we're going to be adding to the translated line
+					boolean stillReading = true;	//keep reading codons until we 
+													//get to end of line 
+													//or a STOP codon
+					int codonCount = 1;				//count of how many codons 
+													//have been read
+					String acidAddition = "";		//the amino chain we're going 
+													//to be adding to the 
+													//translated line
 					while(stillReading)
 					{
 						//get the next codon in the chain
-						String codon = s.substring(lineStart + 3 * codonCount, lineStart + 3 * codonCount + 3); 
+						String codon = s.substring(lineStart + 3 * codonCount,
+											lineStart + 3 * codonCount + 3); 
 						
 						String val = "";	//value will store the translated value
-											//traverse from the back, so that we can see if it stops first
+											//traverse from the back, so that we 
+											//can see if it stops first
 						for(int z = translations.size()-1; z >= 0; z--)		
-							if(translations.get(z).indexOf(codon) != -1 && val == "" && z != indexOfStarts)		
+							if(translations.get(z).indexOf(codon) != -1 
+								&& val == "" && z != indexOfStarts)		
 								val = extractCodons(translations.get(z))[0];	
 						//if the codon exists in one of the 
 						//translation lines, set 'val' to the value before the *. 
 						//Also make sure not to read anything as a 'START'
 						
 						if(val.equals("STOP"))
-							stillReading = false; //if it's a stop, get out of reading
+							stillReading = false; //if it's a stop, 
+												  //get out of reading
 						else
-							acidAddition += val;  //else, add the codon to the current chain
+							acidAddition += val;  //else, add the codon 
+												  //to the current chain
 						
-						//increment the number of codons we've counted, so we can count the next one
+						//increment the number of codons we've counted, 
+						//so we can count the next one
 						codonCount++;			
 						if(!(lineStart + 3 * codonCount + 3 <= s.length()))
 							stillReading = false; //if we're out of bounds, stop
 					}
 					
-					//if the acid addition was empty, we've got an START-STOP or START-END
+					//if the acid addition was empty, we've got a 
+					//START-STOP or START-END
 					if(acidAddition.equals(""))		
 						aminoAcids += "NULL";
-					else							//else add the current chain to the current line
+					else							//else add the current chain 
+													//to the current line
 						aminoAcids += acidAddition;
 					aminoAcids += " ";				//space after
 					
-					//we're going to cut out the chain we just read and everything before it
+					//we're going to cut out the chain we just 
+					//read and everything before it
 					//this is the index of the next codon
 					currentLineStart = lineStart + 3 * codonCount;	
 				}
 				else
-					stillInLine = false;	//if we didn't find any START, end the line read
+					stillInLine = false;	//if we didn't find any START, 
+											//end the line read
 				if(currentLineStart + 2 > s.length())
 					stillInLine = false;
 			}
