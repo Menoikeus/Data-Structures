073771bbb5576328e51a5830918f0c35cd44d090
Menoikeus <commander1347@hotmail.com>
2017-01-12 11:54:30 -0500

    Comments for Mikey

--- MikeyDN/src/MikeyDN.java
+++ MikeyDN/src/MikeyDN.java
@@ -3,10 +3,12 @@ import javax.swing.JOptionPane;
 public class MikeyDN {
 	public static void main(String[] args)
 	{
+		// table heading
 		System.out.format("%-36s%-36s%12s%n", "Postfix", "Infix", "Result");
 		
+		// create string s to take in input, and check if it's 'quit'. if so, stop, if not, evaluate s
 		String s;
-		while(!(s = JOptionPane.showInputDialog(null, "Postfix Expression: ")).toLowerCase().equals("quit"))
+		while(!(s = JOptionPane.showInputDialog(null, "Postfix Expression (or 'quit' to quit): ")).toLowerCase().equals("quit"))
 		{
 			PostfixEvaluator.Evaluate(s);
 		}
--- MikeyDN/src/PostfixEvaluator.java
+++ MikeyDN/src/PostfixEvaluator.java
@@ -8,7 +8,8 @@ public class PostfixEvaluator {
 		String[] values = s.split(" "); // splits input into array
 		String operations = "+-*/";
 		
-		int numNumbers = 0, numOperations = 0;
+		int numNumbers = 0, numOperations = 0; //counts the number of operations
+												// and numbers
 		for(String aString : values)
 		{
 			if(operations.indexOf(aString) != -1)
@@ -18,25 +19,33 @@ public class PostfixEvaluator {
 		}
 		
 		boolean error = false, divideByZero = false; // two types of errors: input and divide by zero
-		if(numNumbers == numOperations + 1)
+		if(numNumbers == numOperations + 1) // if there's one more number than operations
 		{
-			ArrayList<String> outputValues = new ArrayList<String>();
-			for(int i = 0; i < values.length && !error; i++)
+			ArrayList<String> outputValues = new ArrayList<String>(); // arraylist for output
+			int i = -1;
+			while(++i < values.length && !error && !divideByZero) // start at 0 and go through the 
+																// input array, until there's an error
 			{
-				String ch = values[i];
+				String ch = values[i]; // get the ith value
+				
+				
 				try
 				{
-					stack.push(Double.parseDouble(ch));
-					outputValues.add(ch);
+					stack.push(Double.parseDouble(ch)); // cast value and push to stack
+					outputValues.add(ch); // add the letter to output string arraylist
 				}
 				catch(Exception e)
 				{
-					if(!stack.isEmpty())
+					if(!stack.isEmpty()) // if there's something in the stack
 					{
+						// get the top of the stack
 						double latest = ((Double)stack.pop()).doubleValue();
-						if(!stack.isEmpty())
+						if(!stack.isEmpty()) // if after one push there's still something, do this
+											// if not, we've got an error
 						{
 							double secondLatest = ((Double)stack.pop()).doubleValue();
+							
+							// do operation
 							if(ch.equals("+"))
 								stack.push(secondLatest + latest);
 							if(ch.equals("-"))
@@ -45,16 +54,19 @@ public class PostfixEvaluator {
 								stack.push(secondLatest * latest);
 							if(ch.equals("/"))
 							{
+								//if you're dividing by zero
 								if(Math.abs(latest) >= 0.0000001)
 									stack.push(secondLatest / latest);
 								else
 									divideByZero = true;
 							}
 							
-							if(!error && !divideByZero)
+							if(!error && !divideByZero) // if we haven't errored, then add to output
 							{
 								String secondValue = outputValues.remove(outputValues.size() - 1);
 								String firstValue = outputValues.remove(outputValues.size() - 1);
+								
+								// parentheses
 								outputValues.add("(" + firstValue + " " + ch + " " + secondValue + ")");
 							}
 						}
@@ -65,83 +77,20 @@ public class PostfixEvaluator {
 						error = true;
 				}
 			}
+			// if we haven't errored, print result
 			if(!error && !divideByZero)
-				System.out.format("%-36s%-36s%12.3f%n", s, outputValues.get(0).replace("(", "").replace(")", ""), ((Double)stack.pop()).doubleValue());
-			else if(divideByZero)
+				System.out.format("%-36s%-36s%12.3f%n", s, 
+						outputValues.get(0).indexOf("(") != -1 ? 
+								outputValues.get(0).substring(1, outputValues.get(0).length() - 1) : 
+								outputValues.get(0), 
+									((Double)stack.pop()).doubleValue());
+				// if there are parentheses, then remove the end ones. If not, just print
+			else if(divideByZero) // else if / by 0, say so
 				System.out.format("%-36s%-36s%12s%n", s, "UNDEFINED", "UNDEFINED");
-			else if(error)
+			else if(error) // if error, error
 				System.out.format("%-36s%-36s%12s%n", s, "INVALID", "INVALID");
 		}
-		else
+		else // say error if the beginning input was bad (not enough operators)
 			System.out.format("%-36s%-36s%12s%n", s, "INVALID", "INVALID");
-		
-		
-		
-		/*
-		String whole = "";
-		ArrayList<String> alValues = new ArrayList<String>(Arrays.asList(values));
-		for(String s2 : alValues)
-		{
-			System.out.println("a:" + s2);
-		}
-		//for(String z : alValues)
-			//System.out.println(z);
-		
-		int i = alValues.size() - 1;
-		while(i > 0)
-		{
-			if(operations.indexOf(alValues.get(i)) != -1)
-			{
-				if(whole.isEmpty())
-				{
-					System.out.println(i + " " + alValues.size());
-					whole = alValues.get(i-2) + " " + alValues.get(i) + " " + alValues.get(i-1);
-					alValues.remove(i-2);
-					alValues.remove(i-2);
-					alValues.remove(i-2);
-					i -= 2;
-					System.out.println(whole);
-				}
-				else
-				{
-					whole += AddParentheses(whole) + " " + alValues.get(i) + " " + alValues.get(i-1);
-					alValues.remove(i-2);
-					alValues.remove(i-2);
-					i -= 1;
-				}
-			}
-			i--;
-			//System.out.println(whole);
-		}
-		
-		String output = "";
-		for(String os : outputValues)
-		{
-			output += os;
-		}
-		
-		System.out.println(whole);*/
 	}
 }
-
-/*
- * if(first)
-				{
-					/*outputValues.add(secondLatest + ch + latest);
-					/*
-					outputValues.add(secondLatest + "");
-					outputValues.add(ch + "");
-					outputValues.add(latest + "");
-					
-					outputString = secondLatest + " " + ch + " " + latest;
-					first = false;
-				}
-				else
-				{
-					//if(operations.indexOf(outputValues.get(outputValues.size()-2)) != -1)
-					{
-						outputString = AddParentheses(outputString) + " " + ch + " " + secondLatest; 
-						//outputValues.add(ch + " " + latest);
-					}
-				}
-				*/
