c5f5d7bb62bfc57ba93b22323792c50c72f0719d
Menoikeus <commander1347@hotmail.com>
2017-03-07 12:02:29 -0500

    Wrapped up ACSL3

--- ACSL3DNguyen/src/ACSL3DNguyen.java
+++ ACSL3DNguyen/src/ACSL3DNguyen.java
@@ -5,31 +5,38 @@ public class ACSL3DNguyen {
 	{
 		while(true)
 		{
-			boolean[][] board = new boolean[8][8];
-			String input = JOptionPane.showInputDialog(null, "INPUT: ");
-			String[] vals = input.split(" ");
-			
-			int numSets = Integer.parseInt(vals[0]);
-			for(int i = 1; i <= numSets; i++)
+			try
 			{
-				int row = Integer.parseInt(vals[i].substring(0, 1)) - 1; //-1
-				for(int j = 1; j < vals[i].length(); j++)
-					board[row][Integer.parseInt(vals[i].substring(j, j+1)) - 1] = true; //-1
+				boolean[][] board = new boolean[8][8];
+				String input = JOptionPane.showInputDialog(null, "INPUT: ");
+				String[] vals = input.split(" ");
+				
+				int numSets = Integer.parseInt(vals[0]);
+				for(int i = 1; i <= numSets; i++)
+				{
+					int row = Integer.parseInt(vals[i].substring(0, 1)) - 1; //-1
+					for(int j = 1; j < vals[i].length(); j++)
+						board[row][Integer.parseInt(vals[i].substring(j, j+1)) - 1] = true; //-1
+				}
+				
+				for(int i = numSets + 2; 
+						i <= numSets + 1 + Integer.parseInt(vals[numSets + 1]); i++)
+					press(board, 
+						Integer.parseInt(vals[i].substring(0, 1))-1, 
+						Integer.parseInt(vals[i].substring(1, 2))-1);
+				
+				int counter = 0;
+				for(int i = board.length - 1; i >= 0; i--)
+					for(int j = 0; j < board[i].length; j++)
+						if(board[i][j])
+							counter++;
+				
+				System.out.println(counter);
 			}
-			
-			for(int i = numSets + 2; 
-					i <= numSets + 1 + Integer.parseInt(vals[numSets + 1]); i++)
-				press(board, 
-					Integer.parseInt(vals[i].substring(0, 1))-1, 
-					Integer.parseInt(vals[i].substring(1, 2))-1);
-			
-			int counter = 0;
-			for(int i = board.length - 1; i >= 0; i--)
-				for(int j = 0; j < board[i].length; j++)
-					if(board[i][j])
-						counter++;
-			
-			System.out.println(counter);
+			catch(Exception e)
+			{
+				
+			}
 		}
 	}
 	
@@ -37,7 +44,7 @@ public class ACSL3DNguyen {
 	{
 		for(int i = 0; i < b.length; i++)
 			for(int j = 0; j < b[i].length; j++)
-				if(Math.pow(i - row, 2) + Math.pow(j - col, 2) <= 4)
+				if(Math.abs(i - row) + Math.abs(j - col) <= 2)
 					b[i][j] = b[i][j] ? false : true;
 	}
 }
